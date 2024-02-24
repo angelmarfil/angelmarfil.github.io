b4740761538ebf369e1f706f38d887d01f2745da
Angel Marfil <angelmarfil11@gmail.com>
2024-02-23 23:56:54 -0500

    Added custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://angelmarfil.github.io',
+  site: 'https://angelmarfil.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
